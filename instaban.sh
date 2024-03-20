8e3794f5000c292024799180acdbc201588c4f32
SHIBIL143 <103309836+SHIBIL143@users.noreply.github.com>
2024-03-20 23:10:42 +0530

    instaban.sh

--- instaban.sh
+++ instaban.sh
@@ -4,18 +4,21 @@ clear
 sleep 1
 echo Subscribe Please
 sleep 3
-xdg-open "https://youtube.com/@SHIBILGAMER"
+xdg-open "https://www.youtube.com/@SHIBILGAMER"
 sleep 1
 echo loading...
 sleep 10
-apt update -y
-apt upgrade
-pkg update -y
-pkg upgrade
 pkg install python -y
 pkg install python2 -y
 pkg install python3 -y
 pkg install curl -y
 pkg install figlet
+pip install requests
+pkg install imagemagick -y
 sleep 1
+echo loading....
+touch u.txt
+touch p.txt
+touch v.txt
+sleep 2
 bash local.sh
